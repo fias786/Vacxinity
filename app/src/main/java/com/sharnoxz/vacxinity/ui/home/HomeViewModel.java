adea82a562c55d100baf634c583b562e6305f770
Saif Ali <45361119+fias786@users.noreply.github.com>
2021-04-11 02:30:35 +0530

    Home ViewModel Update

--- app/src/main/java/com/sharnoxz/vacxinity/ui/home/HomeViewModel.java
+++ app/src/main/java/com/sharnoxz/vacxinity/ui/home/HomeViewModel.java
@@ -4,16 +4,19 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+
 public class HomeViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private MutableLiveData<ArrayList<VerticalData>> data;
 
     public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        data = new MutableLiveData<>();
+        DataSources DataSources = new DataSources();
+        data.setValue(DataSources.loadVerticalData());
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<ArrayList<VerticalData>> getData() {
+        return data;
     }
 }
