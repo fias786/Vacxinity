8c9de7f6e2a8533049af910a4b228e9922e0019a
Saif Ali <45361119+fias786@users.noreply.github.com>
2021-04-11 02:31:39 +0530

    Dashboard Fragment Update

--- app/src/main/java/com/sharnoxz/vacxinity/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/sharnoxz/vacxinity/ui/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.sharnoxz.vacxinity.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,26 +9,40 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.sharnoxz.vacxinity.R;
+import com.sharnoxz.vacxinity.Scheduling;
+import com.sharnoxz.vacxinity.Vaccination;
+import com.sharnoxz.vacxinity.accounts.SharedPrefs;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    CardView cardView1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        cardView1 = root.findViewById(R.id.CardView1);
+        cardView1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                Boolean check = Boolean.valueOf(SharedPrefs.readSharedSetting(getContext(),"Scheduling","true"));
+                if(check) {
+                    Intent intent = new Intent(getContext(), Scheduling.class);
+                    startActivity(intent);
+                }
+                else {
+                    Intent intent = new Intent(getContext(), Vaccination.class);
+                    startActivity(intent);
+
+                }
             }
         });
         return root;
